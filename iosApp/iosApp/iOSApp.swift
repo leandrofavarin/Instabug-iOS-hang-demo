9c970dd6ba87c156c97679cdebd5cfe95f0df674
Leandro Favarin <lfavarin@gmail.com>
2023-12-07 11:27:57 +0100

    Add Instabug v12.4.0 as SP dependency

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import Instabug
 
 @main
 struct iOSApp: App {
+  init() {
+    Instabug.start(withToken: "any value hangs the app, empty correctly crashes", invocationEvents: .shake)
+  }
+
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
